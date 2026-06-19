604a465105863c85e00d21732163fe0503231f8b
swethajagadeeswaran05-del <swethajagadeeswaran05@gmail.com>
2026-06-19 10:52:25 +0530

    Delete onetoone/onetoone directory

--- onetoone/onetoone/src/main/java/com/example/onetoone/StudentProfileRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.onetoone;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentProfileRepository extends JpaRepository<StudentProfile, Long> {}
--- onetoone/onetoone/src/main/java/com/example/onetoone/StudentRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.onetoone;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {}
